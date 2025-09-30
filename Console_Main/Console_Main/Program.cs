fb8291f87691f7975a079d7285d76f4c432d9016
Leo Zwindrecht <leozwindrecht@outlook.com>
2025-09-30 22:22:11 +0200

    Test 2?

---
+++ Console_Main/Console_Main/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Console_Main;
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+
+char valueToDetect = 's';
+
+string[] words = { "aLorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy",
+"eirmod atempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et",
+"accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem"};
+
+string[] words2 = { "aLorem aipsum adolor asit aamet, consetetur asadipscing elitr, sed diam nonumy",
+"eirmod atempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et",
+"accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem"};
+
+
+DoesItStartWithX.LoopThatDetectsIfSomethingStartsWithX(words2, valueToDetect);
+DoesItStartWithX.LoopThatDetectsIfSomethingStartsWithX(words2, 'a');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Console_Main/Console_Main/Scripts/DoesItStartWithX.cs
@@ -0,0 +1,18 @@
+public class DoesItStartWithX{
+
+    public static void LoopThatDetectsIfSomethingStartsWithX(string[] inputWords, char valueToDetect)
+    {
+        foreach (string line in inputWords)
+        {
+            string[] elements = line.Split(" "); // splits sentence when space is detected
+            foreach (string elem in elements)
+            {
+                if (elem.StartsWith(valueToDetect)) //tries to valueToDetect in each elem from elements
+                {
+                    Console.WriteLine(elem);
+                }
+            }
+
+        }
+    }
+}
