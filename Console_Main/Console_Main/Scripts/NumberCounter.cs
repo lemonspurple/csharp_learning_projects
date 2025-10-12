c08275d70bb019de88185b67ba6a7ddfc91953c7
Leo Zwindrecht <leozwindrecht@outlook.com>
2025-10-12 20:10:45 +0200

    clean up

--- Console_Main/Console_Main/Scripts/NumberCounter.cs
+++ Console_Main/Console_Main/Scripts/NumberCounter.cs
@@ -7,23 +7,25 @@ class NumberCounter
         int counterOfElements = input.Count();
         int loopCounter = 0;
         int[] scale = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-        // Console.WriteLine($"\n\nTotal number of elements for analasys: {counterOfElements}"); // WRITES OUT THE LIST AS STRING 
         string combinedResults = DoubleToStringHelper(input);
 
         // Console.WriteLine(combinedResults);
-        Console.WriteLine("\n"+"--------------------------------");
-                foreach (int element in scale)
-         {
+        Console.WriteLine("\n" + "--------------------------------");
+        foreach (int element in scale)
+        {
 
-             string stringToBeAnalyzed = combinedResults;
+            string stringToBeAnalyzed = combinedResults;
 
-             int count = stringToBeAnalyzed.Length - stringToBeAnalyzed.Replace(scale[loopCounter].ToString(), "").Length;
-             Console.WriteLine($"The numer {scale[loopCounter]} appeared {count} times."); 
-             if(loopCounter < 9)
-             {
-                 loopCounter++;
-             }
-         }
+            int count = stringToBeAnalyzed.Length - stringToBeAnalyzed.Replace(scale[loopCounter].ToString(), "").Length;
+            // total number of chars in string - (total number of chars in string - how often the number occured) = total number of occurances
+
+
+            Console.WriteLine($"The numer {scale[loopCounter]} appeared {count} times.");
+            if (loopCounter < 9)
+            {
+                loopCounter++;
+            }
+        }
     }
 
     static string DoubleToStringHelper(List<double> input)
